8df3181ea5f5355dfd570483e7be78fcdbbb48d8
Dharanidadamoni <54370176+Dharanidadamoni@users.noreply.github.com>
2022-06-04 08:35:49 +0530

    Update README.js

--- README.js
+++ README.js
@@ -8,4 +8,37 @@
 Dharanidadamoni/Dharanidadamoni is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
-console.log("Dharani");
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function printLettersBetween(alphabet, startLetter, endLetter) {
+  // Print the letter
+  console.log(startLetter, endLetter);
+
+  // Base Case: Stop the program when startLetter equals endLetter
+  if (startLetter == endLetter) {
+    return startLetter;
+  }
+  startI = alphabet.indexOf(startLetter);
+  endI = alphabet.indexOf(endLetter);
+  // If the starting letter is after than the ending
+  // letter, decrease the index by one. Otherwise, increase
+  // the index by one.
+  if(startI > endI){
+    console.log(startLetter)
+    startI = startI -1;
+    startLetter = alphabet[startI];
+  } else {
+    console.log(startLetter)
+    startI = startI + 1;
+    startLetter = alphabet[startI]
+  }
+
+  // Make a recursive call with the modified letter.
+  return printLettersBetween(alphabet, startLetter, endLetter);
+}
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try {
+  module.exports = printLettersBetween;
+} catch {
+  module.exports = null;
+}
